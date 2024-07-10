bf7b7879d66ae929897a3a8b92e23a11a00da764
Michael Ajwani <michael.ajwani@i22.de>
2024-07-10 21:09:31 +0200

    added new required attributes to Airplane

--- airplane.rb
+++ airplane.rb
@@ -1,10 +1,15 @@
 class Airplane
-  # declare instance-variables with getter and setter methods out of the box (Ruby-Magic!)
-  attr_accessor :model, :capacity, :cargo_load, :cruising_range
+  # declare instance-variables with getter and optional setter methods out of the box (Ruby-Magic!)
+  attr_reader :model, :capacity, :cargo_load, :cruising_range
+  attr_reader :identifier, :flight_readiness, :performed_maintenance_count
+  attr_accessor :pilot, :co_pilot
 
   # constructor in Ruby
   # we are raising an exception here, cause we only want to allow instances from a concrete subclass!
   def initialize
+    @flight_readiness = false
+    @performed_maintenance_count = nil
+
     raise 'Instance not allowed. Use a subclass of Airplane!'
   end
 
