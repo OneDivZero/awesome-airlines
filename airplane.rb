40e305c3e8a6c5c03c032dca81ddefb2c9454a3b
Michael Ajwani <michael.ajwani@i22.de>
2024-07-10 18:22:36 +0200

    coded :to_s on airplane

--- airplane.rb
+++ airplane.rb
@@ -4,4 +4,8 @@ class Airplane
   def initialize
     raise 'Instance not allowed. Use a subclass of Airplane!'
   end
+
+  def to_s
+    "#{self.class.name} #{@model} | capacity: #{@capacity} | cargo_load: #{@cargo_load} | cruising_range: #{@cruising_range}"
+  end
 end
