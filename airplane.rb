c13453c1f9ad766e501b2d694a8b86d6d7bb36ba
Michael Ajwani <michael.ajwani@i22.de>
2024-07-10 20:26:31 +0200

    improved doc | Issue #story

--- airplane.rb
+++ airplane.rb
@@ -1,10 +1,14 @@
 class Airplane
+  # declare instance-variables with getter and setter methods out of the box (Ruby-Magic!)
   attr_accessor :model, :capacity, :cargo_load, :cruising_range
 
+  # constructor in Ruby
+  # we are raising an exception here, cause we only want to allow instances from a concrete subclass!
   def initialize
     raise 'Instance not allowed. Use a subclass of Airplane!'
   end
 
+  # override :to_s provided by class Object
   def to_s
     "#{self.class.name} #{@model} | capacity: #{@capacity} | cargo_load: #{@cargo_load} | cruising_range: #{@cruising_range}"
   end
